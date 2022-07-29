42774238e3a53a087692424e3fbd9c387c052d73
Zeitgeist13 <giorgikukhaleishvili13@gmail.com>
2022-07-29 22:26:37 +0400

    https://github.com/Zeitgeist13/first-workshop-js.git

---
+++ index.js/index.js
@@ -0,0 +1,83 @@
+let user={
+    name:"Ana",
+    Age:28,
+    sibling:{
+        name:"Levan",
+        Age1:21,
+        brother:true,
+        sister:false,
+    },
+
+};
+let sum=user.Age-Age1;
+console.log(sum);
+let result='Ana is 7 years older then Levan';
+
+
+
+
+
+
+let studentsArr=['John','Sam','Ann','Will','Andrew','Joseph','Lorelai','Amelie','Dan'
+
+];
+let middleelement=[studentsArr.length -1/ 2]
+console.log(middleelement);
+
+
+
+
+
+
+
+
+ let studentuser={
+    Firstname:'Alex',
+    Lastname:'Morgan',
+    Age:21,
+    SubjectsArr:['International Politics','Philosophy', 'Cultural Relativism'],
+    Roommate:{
+        Name:'Bobby',
+        Surname:'Watson',
+        Age:'22',
+    }
+
+}
+for(let i=0; i<studentuser.SubjectsArr.length; i++){
+    console.log(SubjectsArr[i])
+};
+let fullname=studentuser.Firstname+studentuser.Lastname;
+console.log(fullname);
+
+let roommatefullname=roommate.Name+roommate.surname;
+console.log(fullname2);
+
+let result1='Fullname'+studentuser.age+roommatefullname;
+
+
+
+
+
+
+let fruitsArr=['Banana', 'orange', 'Apple', 'Mango',2, 12
+];
+
+while (
+    let (i=0,i<fruitsArr.length-1,i++)
+)
+
+
+
+ 
+let (numbersArr)=[12,23,43,11,9,2,121,90];
+
+
+for (let i=0; i<numbersArr;i++) {
+    
+    if (numbersArr[i]>31, numbersArr[i]%2>0)
+    console.log('element is greater than provided value and is EVEN')
+   
+    else('element is less than provided value and is ODD')
+};
+
+
